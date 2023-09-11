6af6183956d7c47e74fb7dec5a17e8b99fd8162a
Matheus França <115188878+MatheusFrancaDEV@users.noreply.github.com>
2023-09-11 07:55:45 -0300

    Fixing script

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,21 @@ let localidade = document.getElementById('localidade');
 let uf = document.getElementById('uf');
 const btnConsult = document.getElementById('consultar');
 
+function verificedValues(){{
+    let labelFor = document.querySelectorAll('label');
+
+    if(logradouro.value == ""){
+        logradouro.style.display = "none";
+        bairro.style.display = "none";
+    }
+    else{
+        labelFor[1].style.display = "block";
+        labelFor[2].style.display = "block";
+        logradouro.style.display = "block";
+        bairro.style.display = "block";
+    }
+}}
+
 btnConsult.addEventListener('click', async (event) =>{
     event.preventDefault();
 
@@ -15,4 +30,7 @@ btnConsult.addEventListener('click', async (event) =>{
     bairro.value = resposta.bairro;
     localidade.value = resposta.localidade;
     uf.value = resposta.uf;
+
+    console.log(resposta)
+    verificedValues();
 })
